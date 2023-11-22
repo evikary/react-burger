5d5b10f7bc687b843d45ecab5b3e13bafb77bf11
evikary <daria_more10@mail.ru>
2023-11-22 14:24:59 +0300

    feat: add app-header block

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import style from './app-header.module.css';
+
+function AppHeader() {
+
+    const Link = (props: any) => {
+        return (
+            <a className={style.link} href="#">
+                <>{props.img}</>
+                <span className={!props.inActiv ? "ml-2 text text_type_main-default" : "ml-2 text text_type_main-default text_color_inactive"}>{props.children}</span>
+            </a>
+        )
+
+    }
+
+    return (
+        <header className={`${style.header} pt-4 pb-4`}>
+            <nav className={style.navigation}>
+                <ul className={style.navigation_panel}>
+                    <li className='pt-4 pb-4 pl-5 pr-5 mr-2'>
+                        <Link img={<BurgerIcon type="primary" />}>Конструктор</Link>
+                    </li>
+                    <li className='pt-4 pb-4 pl-5 pr-5'>
+                        <Link inActiv img={<ListIcon type="primary" />}>Лента заказов</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Logo></Logo>
+            <div className={`${style.profile} pt-4 pb-4 pl-5 pr-5`}>
+                <Link inActiv img={<ProfileIcon type="primary" />}>Личный кабинет</Link>
+            </div>
+        </header>
+    );
+}
+
+export default AppHeader;
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import AppHeader from '../app-header/app-header';
 
 function App() {
   return (
     <div className="App">
+      <AppHeader></AppHeader>
     </div>
   );
 }
