460147dac6a873449dbe758020f91fddcd5d051b
evikary <daria_more10@mail.ru>
2023-11-23 20:40:36 +0300

    feat: add types for props

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,35 +1,29 @@
-import React from "react";
+import React from 'react';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import Link from '../link/link';
 import style from './app-header.module.css';
 
 function AppHeader() {
-
-    const Link = (props: any) => {
-        return (
-            <a className={style.link} href="#">
-                <>{props.img}</>
-                <span className={!props.inActiv ? "ml-2 text text_type_main-default" : "ml-2 text text_type_main-default text_color_inactive"}>{props.children}</span>
-            </a>
-        )
-
-    }
-
     return (
         <header className={style.header}>
             <div className={`${style.header_wrapper} pt-4 pb-4`}>
                 <nav className={style.navigation}>
                     <ul className={style.navigation_panel}>
-                        <li className='pt-4 pb-4 pl-5 pr-5 mr-2'>
+                        <li className="pt-4 pb-4 pl-5 pr-5 mr-2">
                             <Link img={<BurgerIcon type="primary" />}>Конструктор</Link>
                         </li>
-                        <li className='pt-4 pb-4 pl-5 pr-5'>
-                            <Link inActiv img={<ListIcon type="secondary" />}>Лента заказов</Link>
+                        <li className="pt-4 pb-4 pl-5 pr-5">
+                            <Link isActive img={<ListIcon type="secondary" />}>
+                                Лента заказов
+                            </Link>
                         </li>
                     </ul>
                 </nav>
                 <Logo></Logo>
                 <div className={`${style.profile} pt-4 pb-4 pl-5 pr-5`}>
-                    <Link inActiv img={<ProfileIcon type="secondary" />}>Личный кабинет</Link>
+                    <Link isActive img={<ProfileIcon type="secondary" />}>
+                        Личный кабинет
+                    </Link>
                 </div>
             </div>
         </header>
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import style from './app.module.css';
 import AppHeader from '../app-header/app-header';
-import BurgerIngridients from '../burger-ingridients/burger-ingridients';
+import BurgerIngridients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
   return (
-    <div className="App">
-      <AppHeader></AppHeader>
+    <>
+      <AppHeader />
       <main className={style.main}>
-        <BurgerIngridients></BurgerIngridients>
-        <BurgerConstructor></BurgerConstructor>
+        <BurgerIngridients />
+        <BurgerConstructor />
       </main>
-    </div>
+    </>
   );
 }
 
