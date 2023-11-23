22d542b6b4d03f54ef7ee8bbabc18a8ab375461f
evikary <daria_more10@mail.ru>
2023-11-23 13:17:18 +0300

    feat: add burger-constructor

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngridients from '../burger-ingridients/burger-ingridients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <AppHeader></AppHeader>
       <main className={style.main}>
         <BurgerIngridients></BurgerIngridients>
+        <BurgerConstructor></BurgerConstructor>
       </main>
     </div>
   );
--- src/components/ingridients-category/ingridients-category.tsx
+++ src/components/ingridients-category/ingridients-category.tsx
@@ -2,16 +2,14 @@ import style from './ingridients-category.module.css';
 import IngridientDetail from '../ingridient-details/ingridient-detail';
 
 function IngridientsСategory(props: any) {
-    console.log('f', props);
     return (
         <div className='pb-10'>
             <h2 className="text text_type_main-medium">{props.children}</h2>
             <ul className={`${style.container} pt-6 pl-4 pr-4`}>
                 {props.ingridients.map((item: any) => {
-                    console.log('item', item)
                     return (
-                        <li className={style.item}>
-                            <IngridientDetail key={item._id} image={item.image} name={item.name} price={item.price}></IngridientDetail>
+                        <li key={item._id} className={style.item}>
+                            <IngridientDetail image={item.image} name={item.name} price={item.price}></IngridientDetail>
                         </li>
                     )
                 })}
